c2f12c8440f5fcda71121ee0b17b97737a61cbd5
WinterC0d3r <informatics.hex@gmail.com>
2017-12-31 15:05:12 -0800

    Search condition applies

--- App.py
+++ App.py
@@ -2,11 +2,18 @@
 English Dictionary, created in python and json. for lightweigh use via terminal
 """
 import json
+import difflib
+from difflib import SequenceMatcher
+from difflib import get_close_matches
+
 # loading json data file containing Dictionary dataSource
 sourceData = json.load(open("DataSource.json"))
 def wordSearch(enterKey):
-    return sourceData[enterKey]
-
+    if enterKey in sourceData:
+        return sourceData[enterKey]
+    else:
+        return "Word no found, Please recheck"
+        
 # input var for lookup
 searchInput = input("Enter a word: ")
 # printing results
