005e47d7649e8cf07cd6d1042fc30981929eb970
WinterC0d3r <informatics.hex@gmail.com>
2017-12-31 16:20:45 -0800

    Input Validation updated

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 """
-English Dictionary, created in python and json. for lightweigh use via terminal
+English Dictionary, created in python and json. for lightweight use via terminal
 """
 import json
 import difflib
@@ -8,12 +8,16 @@ from difflib import get_close_matches
 
 # loading json data file containing Dictionary dataSource
 sourceData = json.load(open("DataSource.json"))
-def wordSearch(enterKey):
-    if enterKey in sourceData:
-        return sourceData[enterKey]
+# function for searching word
+def wordSearch(enteredKey):
+    if enteredKey in sourceData:
+        return sourceData[enteredKey]
+    # incase of typo, getting possible match
+    elif len(get_close_matches(enteredKey, sourceData.keys())) > 0:
+        return "Did you mean: %s " % get_close_matches(enteredKey, sourceData.keys())[0]
     else:
-        return "Word no found, Please recheck"
-        
+        return "Word not found, Please recheck"
+
 # input var for lookup
 searchInput = input("Enter a word: ")
 # printing results
