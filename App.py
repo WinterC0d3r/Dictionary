7c3734ef1bbc8b53098fde096f90c738b3c2cbe3
WinterC0d3r <informatics.hex@gmail.com>
2017-12-31 18:45:56 -0800

    Results optimized

--- App.py
+++ App.py
@@ -8,24 +8,33 @@ from difflib import get_close_matches
 
 # loading json data file containing Dictionary dataSource
 sourceData = json.load(open("DataSource.json"))
+
 # function for searching word
 def wordSearch(enteredKey):
     if enteredKey in sourceData:
         return sourceData[enteredKey]
+
     # incase of typo, getting possible match
     elif len(get_close_matches(enteredKey, sourceData.keys())) > 0:
         varValidate = input ("Did you mean: %s\nType \"Y\" for yes, \"N\" for no. " % get_close_matches(enteredKey, sourceData.keys())[0])
+
+        #input validation
         if varValidate == "Y":
             return sourceData[get_close_matches(enteredKey, sourceData.keys())[0]]
         elif varValidate == 'N':
             return "Did not find a match"
         else:
             return "You did not select a valid option"
-
     else:
         return "Word not found, Please recheck...."
 
-# input var for lookup
+# input var for search
 searchInput = input("Enter a word: ")
-# printing results
-print(wordSearch(searchInput.lower()))
+
+# storing output into a variable for optimization
+results = wordSearch(searchInput.lower())
+if type(results) == list:
+    for r in results:
+        print(r)
+else:
+    print(restults)
